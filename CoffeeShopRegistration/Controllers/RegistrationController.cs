e9b35ff3d1f58448e6eef4fb05f720f1141eef6b
Alex Young <alexanderjyoung15@gmail.com>
2023-11-02 08:55:18 -0400

    Add project files.

---
+++ CoffeeShopRegistration/Controllers/RegistrationController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace CoffeeShopRegistration.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using CoffeeShopRegistration.Models; // Replace with your actual model namespace
+    using Microsoft.AspNetCore.Mvc.ViewEngines;
+   
+    public class RegistrationController : Controller
+    {
+        // Display the registration form
+        public IActionResult ShowRegistrationForm()
+        {
+            return View("views/home/Registration.cshtml");
+        }
+
+        // Process user registration
+        [HttpPost]
+        public IActionResult User()
+        {
+            User a = new User()
+            {
+                fname = "Test",
+                lname = "Test",
+                email = "Test",
+                pass = "Test",
+            }
+        }
+
+        public IActionResult Register(string fname, string lname, string email, string pass)
+        {
+           
+
+            if (ModelState.IsValid)
+            {
+                // Process user registration here
+                // You can save user data, perform validation, etc.
+
+                // Assuming you have a UserRegistrationModel for the form fields
+                // This model should match the form fields in your Registration.cshtml view
+
+               
+
+                // Redirect to the result page and pass the user's first name
+                return View("/views/home/Result.cshtml", User);
+            }
+            else
+            {
+                // If there are validation errors, return to the registration form
+                return View("/views/home/Registration.cshtml",User);
+            }
+        }
+    }
+}
+
+
+
---
+++ CoffeeShopRegistration/Models/UserModel.cs
@@ -0,0 +1,15 @@
+﻿
+namespace CoffeeShopRegistration.Models
+{
+    public class User
+    {
+        public string fname { get; set; }
+
+        public string lname { get; set; }
+        public string pass { get; set; }
+        public string email { get; set; }
+
+        public User(){}
+
+    }
+}
